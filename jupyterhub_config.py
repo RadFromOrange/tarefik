950980520b88d3c2b3e2bea4d6328a2768f2f237
exetroncorp <exetron.conseil@gmail.com>
2025-07-01 12:36:14 +0200

    sqsqs

---
+++ jupyterhub_config.py
@@ -0,0 +1,29 @@
+c = get_config()
+
+# Use KubeSpawner
+c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
+c.KubeSpawner.image = 'gitpod/openvscode-server:latest'
+c.KubeSpawner.cmd = ["/openvscode-server/bin/openvscode-server", "--host=0.0.0.0", "--without-connection-token"]
+
+# Traefik-Redis-Proxy configuration
+c.JupyterHub.proxy_class = 'jupyterhub_traefik_proxy.redis.TraefikRedisProxy'
+c.TraefikProxy.traefik_api_url = 'http://traefik-proxy:8080'
+c.TraefikProxy.traefik_api_password = 'your-traefik-api-password'
+c.TraefikProxy.redis_url = 'redis://:your-redis-password@redis:6379/0'
+c.TraefikProxy.provider = 'kubernetescrd'
+c.TraefikProxy.service_name = 'traefik-proxy'
+c.TraefikProxy.tls_verify = False
+
+# Use DummyAuthenticator for testing
+c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+c.DummyAuthenticator.password = 'testpass'
+
+# Other JupyterHub settings
+c.JupyterHub.hub_connect_ip = '0.0.0.0'
+c.JupyterHub.hub_ip = '0.0.0.0'
+c.JupyterHub.hub_port = 8081
+c.JupyterHub.allow_named_servers = True
+c.JupyterHub.admin_access = True
+
+# Example: set admin users
+c.Authenticator.admin_users = {'adminuser'}
