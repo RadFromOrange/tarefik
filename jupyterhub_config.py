7c4ac24c4e944a7288a8c4b1a641a9372b98fa49
chrissavian <chrissavian@gmail.com>
2025-07-01 13:35:32 +0000

    dddd

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -11,8 +11,7 @@ c.KubeSpawner.cmd = ["/openvscode-server/bin/openvscode-server", "--host=0.0.0.0
 c.JupyterHub.proxy_class = 'jupyterhub_traefik_proxy.redis.TraefikRedisProxy'
 c.TraefikRedisProxy.traefik_api_url = 'http://traefik-proxy:8080'
 c.TraefikRedisProxy.traefik_api_password = 'your-traefik-api-password'
-redis_password = os.environ.get('REDIS_PASSWORD', 'test')
-c.TraefikRedisProxy.redis_url = f'redis://:{redis_password}@redis:6379'
+c.TraefikRedisProxy.redis_url = 'redis://redis:6379'
 c.TraefikRedisProxy.should_start = False
 
 # Use DummyAuthenticator for testing
